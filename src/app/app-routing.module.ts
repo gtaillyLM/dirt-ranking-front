7aa0f2802c96a7e6131c8b8b5c80d1d30a11e517
Grégory TAILLY <gregory.tailly@ext.leroymerlin.fr>
2020-12-13 18:46:19 +0100

    :sparkles: add routing home / ranking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { RankingComponent } from './modules/ranking/ranking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ranking', component: RankingComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RankingComponent } from './modules/ranking/ranking.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RankingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
