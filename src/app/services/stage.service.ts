b6a1b06ed53d6154af9d45e64654d1ff3c464797
Grégory TAILLY <gregory.tailly@ext.leroymerlin.fr>
2020-12-27 14:46:16 +0100

    :bug: fix bug prod with backend

--- src/app/services/stage.service.ts
+++ src/app/services/stage.service.ts
@@ -2,15 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RallyStage } from '../modules/ranking/ranking.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StageService {
 
+  baseUrl = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getRallyStage() : Observable<RallyStage[]> {
-    return this.http.get<RallyStage[]>('/api/v1/stage');
+    return this.http.get<RallyStage[]>(this.baseUrl + 'api/v1/stage');
   }
 }
